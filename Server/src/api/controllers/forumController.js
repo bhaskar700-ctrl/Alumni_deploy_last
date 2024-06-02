c5735001cb415b51da08543b85b09b57ccdb5bb6
Bhaskar <bhaskersonowal12@gmail.com>
2024-06-02 23:57:14 +0530

    initial commit

--- Server/src/api/controllers/forumController.js
+++ Server/src/api/controllers/forumController.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import Post from '../models/Post.js';
 import User from '../models/User.js';
-import NotificationController from './NotificationController.js';
+import NotificationController from './notificationController.js';
 
 const ForumController = (io) => ({
     async createPost(req, res) {
